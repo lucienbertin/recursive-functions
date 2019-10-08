d9dd31a3ef3af809bebe5136dbf7c9b290b3f7f9
lucienbertin <me@lucienbertin.com>
2019-10-08 13:54:13 +0200

    first commit with not a lot in it

---
+++ people.js
@@ -0,0 +1,38 @@
+const francois1er = {
+	name: 'francois 1er',
+	children: [
+		'louise de france',
+		'charlotte de france',
+		'francois de france',
+		'henri 2',
+		'madeleine de france',
+		'marguerite de france',
+		'nicolas d estouville',
+	],
+}
+const louiseDeFrance = {
+	name: 'louise de france',
+	children: []
+}
+const charlotteDeFrance = {
+	name: 'charlotte de france',
+	children: []
+}
+const claudeDeFrance = {
+	name: 'claude de france',
+	children: [
+		'louise de france',
+		'charlotte de france',
+		'francois de france',
+		'henri 2',
+		'madeleine de france',
+		'marguerite de france',
+	]
+}
+
+module.exports = [
+	francois1er,
+	claudeDeFrance,
+	louiseDeFrance,
+	charlotteDeFrance
+]
